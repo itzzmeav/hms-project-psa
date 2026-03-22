468b605a3d62e5e221a6f8da80bfc5746ab6026a
Your Name <you@example.com>
2026-03-22 23:21:58 +0530

    first change

--- microservices-1/src/main/java/com/microservices1/controller/WelcomeControllerApi.java
+++ microservices-1/src/main/java/com/microservices1/controller/WelcomeControllerApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeControllerApi {
 	
-	@GetMapping("/message")
+	@GetMapping("/messagess")
 	public String getMessage() {
 		return "welcome";
 	}
